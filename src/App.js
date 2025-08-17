bacf5391388f77cd5b17aaff14031c4f5df3d0f8
Hanna <hannageb@udel.edu>
2025-08-16 22:01:09 -0400

    added text, icons & pictures

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
+import pic from './assets/pic.jpg'
+import github from './assets/github.webp'
+import linkedin from './assets/linkedin.png'
+import mail from './assets/mail.png'
 
 function App() {
   return (
     <>
     <div className="bkgd"></div>
     <div className="card">
-      <div className="pic"><img alt="pic of me!" src="/public/pic.jpg"></img></div>
-      <div className="text">
-        <p>Welcome to my Portfolio!</p>
-        <p>about me</p>
-        <p>education & experience</p>
-        <p>other interests</p>
-      </div>
+      <p style={{justifySelf: 'center'}}>Welcome to my Portfolio!</p>
+      <div className="card-contents">
+        <div className="pic">
+          <img alt="pic of me!" src={pic}></img>
+          <ul className="links">
+            <p>Hanna Gebrel</p>
+            <li href="github.com/hannageb"><img alt="github logo" src={github}></img></li>
+            <li href="linkedin.com/hannagebrel"><img alt="linkedin logo" src={linkedin}></img></li>
+            <li href="gebrelhanna112@gmail.com"><img alt="mail icon" src={mail}></img></li>
+          </ul>
+        </div>
+        <ul className="toggle">
+          <li className="abt">
+            <div className="star"></div>
+            <p>about me</p>
+          </li>
+          <li className="edu">
+            <div className="star"></div>
+            <p>education & experience</p>
+          </li>
+          <li className="misc">
+            <div className="star"></div>
+            <p>other interests</p></li>
+       </ul>
+       </div>
     </div>
     </>
   );
