352c1b1bcbeef91a3ac6581a0532dbba63e3e107
Hanna <hannageb@udel.edu>
2026-07-03 02:26:45 -0400

    download my resume

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import linkedin from './assets/linkedin.png'
 import mail from './assets/mail.png'
 import { useState } from 'react';
 import calendar from './assets/calendar.gif';
-import mesa from './assets/mesa-posts.png';
+import download from './assets/download.svg'
+import resume from './assets/resume.pdf'
+import { resumeToPipeableStream } from 'react-dom/server';
 
 function App() {
   /** for creating the tabs
@@ -20,12 +22,13 @@ function App() {
     <div className="card-container" style={{padding: '10px'}}>
       <div className="tabs">
         <button className={active === 1 ? "active-tab" : ""} onClick={() => setActive(1)}>About Me</button>
-        <button className={active === 2 ? "active-tab" : ""} onClick={() => setActive(2)}>Social Media</button>
-        <button className={active === 3 ? "active-tab" : ""} onClick={() => setActive(3)}>Web Design</button>
+        <button className={active === 2 ? "active-tab" : ""} onClick={() => setActive(2)}>Web Dev</button>
+        <button className={active === 3 ? "active-tab" : ""} onClick={() => setActive(3)}>Design</button>
       </div>
+      {/* FIRST CARD **/}
       <div id="front-card" className={active === 1 ? "show-content" : "content"}>
         <div className="top-text">
-          <p style={{fontWeight: '700px'}}>Welcome to my Portfolio ☺︎</p>
+          <p>Welcome to my Portfolio ☺︎</p>
         </div>
         <div className="card-contents">
           <div className="text">
@@ -38,30 +41,21 @@ function App() {
           </div>
         </div>
         <div className="top-text">
-          <p style={{fontWeight: '700px'}}>Let's Keep in Touch!</p>
+          <p>Let's Keep in Touch!</p>
           <p style={{fontSize: '20px'}}>Feel free to reach out if you have any questions, share film/book recommendations or just want to connect!</p>
             <ul className="links" style={{justifyContent: 'center'}}>
                 <li> <a href="https://www.github.com/hannageb"><img alt="github logo" src={github}></img></a></li>
                 <li> <a href="https://www.linkedin.com/in/hannagebrel"><img alt="linkedin logo" src={linkedin}></img></a></li>
                 <li> <a href="mailto:gebrelhanna112@gmail.com"><img alt="mail icon" src={mail}></img></a></li>
+                <li> <a href={resume} download="Hanna-Gebrel-Resume"><img src={download} alt="download icon"/></a></li>
               </ul>
         </div>
       </div> 
-     <div id="design-card" className={active === 2 ? "show-content" : "content"}>
-        <p style={{justifySelf: 'center', fontWeight: '700px'}}>DESIGN</p>
-        <div className="card-contents">
-          <div className="toggle">
-            <div className="section">
-              <p>experience</p></div>
-                  <div className="inner-section">
-                    <div className="inner-section-text">
-                      <p>A mobile app helping people achieve gender-affirming vocal goals</p>
-                      <p>Skills: React, TypeScript, Figma, User Research Methods</p>
-                    </div>
-                  </div>
-            <div className="section">
-              <p>projects</p> </div>
-                  <><div className="inner-section">
+        {/* WEB DEV CARD **/}
+        <div id="web-development-card" className={active === 2 ? "show-content" : "content"}>
+            <p style={{ justifySelf: 'center', textDecoration: 'underline'}}>WEB DEVELOPMENT</p>
+            <div className="card-contents">
+              <><div className="inner-section">
                     <a href="https://hannageb.github.io/portfolio/">portfolio</a></div>
                       <><div className="inner-section"></div>
                         <div className="inner-section-text">
@@ -80,20 +74,21 @@ function App() {
                           <p>Redesigning the website for the Museum of Islamic Art in Cairo, Egypt</p>
                           <p>Skills: React, TypeScript, Figma</p>
                         </div>
-                      </>
-          </div>
+                      </>"
+            </div>
+        </div>
+        {/* DESIGN CARD **/}
+        <div id="design-card" className={active === 3 ? "show-content" : "content"}>
+          <p style={{justifySelf: 'center'}}>DESIGN</p>
+          <div className="card-contents">
+            <div className="inner-section">
+              <ul className="cards">
+               <li><img src={calendar} alt="cairo calendar" style={{ width: '335px', height: '450px' }} /></li>               
+              </ul>
+            </div>
           </div>
         </div>
-        <div id="web-development-card" className={active === 3 ? "show-content" : "content"}>
-            <p style={{ justifySelf: 'center', fontWeight: '700', textDecoration: 'underline'}}>WEB DEVELOPMENT</p>
-            <div className="card-contents">
-                <ul className="cards">
-                    <li><img src={calendar} alt="cairo calendar" style={{ width: '335px', height: '450px' }} /></li>
-                    <li><img src={mesa} alt="mesa posts" style={{ width: '450px', height: '450px' }} /></li>
-                </ul>
-            </div>
-      </div>
-      </div>
+    </div>
     </>
 
   );
